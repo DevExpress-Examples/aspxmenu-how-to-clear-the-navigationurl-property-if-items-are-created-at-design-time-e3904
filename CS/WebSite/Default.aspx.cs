d2c509ec64079272231eb234e1c323c417153060
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+ 
+using DevExpress.Web.ASPxMenu;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+	protected void ASPxMenu1_Load(object sender, EventArgs e) {
+		ASPxMenu menu = sender as ASPxMenu;
+		
+		//Uncomment the following code to use it in a real application
+		//CorrectItem(menu.RootItem);
+
+		//The following code is used for the demo purpose only. Do not use it in a real scenario
+		CorrectItem(menu.Items[1]);
+	}
+
+	private void CorrectItem(MenuItem item) {
+		if (item == null)
+			return;
+		if (item.HasChildren) {
+			item.NavigateUrl = null;
+			foreach (MenuItem subItem in item.Items)
+				CorrectItem(subItem);
+		}
+	}
+}
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,34 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+
+Imports DevExpress.Web.ASPxMenu
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+
+	End Sub
+	Protected Sub ASPxMenu1_Load(ByVal sender As Object, ByVal e As EventArgs)
+		Dim menu As ASPxMenu = TryCast(sender, ASPxMenu)
+
+		'Uncomment the following code to use it in a real application
+		'CorrectItem(menu.RootItem);
+
+		'The following code is used for the demo purpose only. Do not use it in a real scenario
+		CorrectItem(menu.Items(1))
+	End Sub
+
+	Private Sub CorrectItem(ByVal item As MenuItem)
+		If item Is Nothing Then
+			Return
+		End If
+		If item.HasChildren Then
+			item.NavigateUrl = Nothing
+			For Each subItem As MenuItem In item.Items
+				CorrectItem(subItem)
+			Next subItem
+		End If
+	End Sub
+End Class
